0449374b3f9f6561643b1f79d8857798c9cab370
Kazunari Okuda <saori.cube@gmail.com>
2019-08-02 17:52:42 +0200

    Create JsonAfterMiddleware struct

--- src/handlers.rs
+++ src/handlers.rs
@@ -126,3 +126,12 @@ macro_rules! get_http_param {
         }
     }
 }
+
+pub struct JsonAfterMiddleware;
+
+impl AfterMiddleware for JsonAfterMiddleware {
+  fn after(&self, _: &mut Request, mut res: Response) -> IronResult<Response> {
+    res.headers.set(ContentType::json());
+    Ok(res)
+  }
+}
