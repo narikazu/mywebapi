2c287f2886c6ef95a4a42cb7efba4d0ce4bb4d22
Kazunari Okuda <saori.cube@gmail.com>
2019-08-02 15:27:18 +0200

    Create handlers which includes three macros

---
+++ src/handlers.rs
@@ -0,0 +1,43 @@
+use std::sync::{Arc, Mutex};
+use std::io::Read;
+use iron::{Handler, status, IronResult, Response, Request, AfterMiddleware};
+use iron::headers::ContentType;
+use rustc_serialize::json;
+use database::Database;
+use uuid::Uuid;
+use router::Router;
+use model::Post;
+use std::error::Error;
+
+macro_rules! try_handler {
+    ($e:expr) => {
+        match $e {
+            Ok(x) => x,
+            Err(e) => return Ok(Response::with((status::InternalServerError, e.description())))
+        }
+    };
+    ($e:expr, $error:expr) => {
+        match $e {
+            Ok(x) => x,
+            Err(e) => return Ok(Response::with(($error, e.description())))
+        }
+    }
+}
+
+macro_rules! lock {
+    ($e:expr) => { $e.lock().unwrap()}
+}
+
+macro_rules! get_http_param {
+    ($r:expr, $e:expr) => {
+        match $r.extensions.get::<Router>() {
+            Some(router) => {
+                match router.find($e) {
+                    Some(val) => val,
+                    None => return Ok(Response::with(status::BadRequest)),
+                }
+            }
+            None => return Ok(Response::with(status::InternalServerError)),
+        }
+    }
+}
